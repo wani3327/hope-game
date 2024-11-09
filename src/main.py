b0ad95e4f14beebb0852a9ef32d15278c4c27bb1
man7sym <shimyoungmin9@gmail.com>
2024-11-09 22:42:14 +0900

    changed hog list

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from pygame.locals import *
  
 from mika import Mika
 from bullet import Bullet
+from hog import Hog
 from constants import *
-import hog
 
 class App:
     def __init__(self):
@@ -20,22 +20,24 @@ class App:
         self._mika = Mika()
         self._mika2 = Mika()
         self.bullets: list[Bullet] = [] 
-        self._hog_list = []
-        pygame.time.set_timer(0, 1000)
+        self._hog_list: list[Hog] = []
+        pygame.time.set_timer(0, 1000) # Hog 생성
  
     def on_event(self, event): # 판정
         if event.type == pygame.QUIT:
             self._running = False
 
         if event.type == 0:
-            self._hog_list.append(hog.Hog(self._mika.position))
+            self._hog_list.append(Hog(self._mika.position))
+        
+        if event.type == 1:
+            self.bullets.appdend()
             
     def on_loop(self): # 판정 결과 반영, 틱 이후 진행
         self._mika.update(self.bullets)
         [b.update() for b in self.bullets]
+        [h.update() for h in self._hog_list]
         self._camera = self._mika.position.copy()
-        for i in self._hog_list:
-            i.update()
 
     def on_render(self): # 진행 렌더
         self._display_surf.fill((255, 255, 255))
@@ -43,8 +45,7 @@ class App:
         self._mika2.draw(self._display_surf, self._camera)
         # print(len(self.bullets))
         [b.draw(self._display_surf, self._camera) for b in self.bullets]
-        for i in self._hog_list:
-            i.draw(self._display_surf, self._camera)
+        [h.draw(self._display_surf, self._camera) for h in self._hog_list]
         pygame.display.update()
 
 
